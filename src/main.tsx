9e23177255c556b9c184be80b4842c983e9417f9
함영현 <hyh8414@gmail.com>
2025-11-06 17:32:33 +0900

    최종 수정- 2

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import IframeScenario from './IframeScenario'
+import IframeScenarioAsis from './IframeScenarioAsis'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <IframeScenario />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<IframeScenarioAsis />} />
+        <Route path="/v2" element={<IframeScenario />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
